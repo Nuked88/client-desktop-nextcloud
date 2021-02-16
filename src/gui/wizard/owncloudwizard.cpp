89d039796827f5e1bc1dcffddb022245958b281c
Felix Weilbach <felix.weilbach@nextcloud.com>
2021-02-16 10:32:20 +0100

    Restyle wizard pages
    Signed-off-by: Felix Weilbach <felix.weilbach@nextcloud.com>

--- src/gui/wizard/owncloudwizard.cpp
+++ src/gui/wizard/owncloudwizard.cpp
@@ -52,7 +52,7 @@ OwncloudWizard::OwncloudWizard(QWidget *parent)
     , _httpCredsPage(new OwncloudHttpCredsPage(this))
     , _browserCredsPage(new OwncloudOAuthCredsPage)
     , _flow2CredsPage(new Flow2AuthCredsPage)
-    , _advancedSetupPage(new OwncloudAdvancedSetupPage)
+    , _advancedSetupPage(new OwncloudAdvancedSetupPage(this))
     , _resultPage(new OwncloudWizardResultPage)
     , _webViewPage(new WebViewPage(this))
 {
@@ -284,7 +284,6 @@ void OwncloudWizard::slotCurrentPageChanged(int id)
         auto nextButton = qobject_cast<QPushButton *>(button(QWizard::NextButton));
         if (nextButton) {
             nextButton->setDefault(true);
-            nextButton->setFocus();
         }
     };
 
